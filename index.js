bf9fe56a369efb1b6936d2d1caca46ff26e0a248
Mesuk23 <hadeulislammesuk23@gmail.com>
2022-10-26 01:33:59 +0600

    data link added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const courses = require('./courses.json')
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,6 +8,16 @@ app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('API Running !!!')
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourses = courses.find(n => n.id == id);
+    res.send(selectedCourses);
 })
 
 app.listen(port, () => {
